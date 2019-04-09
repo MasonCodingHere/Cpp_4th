097799b911bdb02426028f84b342a6531dbe946d
xgx127 <xgx127@126.com>
2019-04-09 10:45:31 +0800

    Update to 6_4

---
+++ chapter_06/例6-1 数组的声明与使用.cpp
@@ -0,0 +1,17 @@
+//例6-1.数组的声明与使用
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a[10],b[10];
+	for(int i=0;i<10;i++){
+		a[i]=i*2-1;
+		b[10-i-1]=a[i];
+	}
+	for(int i=0;i<10;i++){
+		cout<<"a["<<i<<"]="<<a[i]<<"  ";
+		cout<<"b["<<i<<"]="<<b[i]<<endl;
+	}
+	return 0;
+} 
