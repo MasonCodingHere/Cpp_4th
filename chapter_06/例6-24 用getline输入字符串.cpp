133e0e50aec0f91569c056701b4c521a1c797e7f
xgx127 <xgx127@126.com>
2019-04-10 14:22:30 +0800

    chapter_06 完毕

---
+++ chapter_06/例6-24 用getline输入字符串.cpp
@@ -0,0 +1,15 @@
+//Àı6-24.cpp ÓÃgetlineÊäÈë×Ö·û´®
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	for(int i=0;i<2;i++){
+		string city,state;
+		getline(cin,city,',');
+		getline(cin,state);
+		cout<<"City: "<<city<<"  State: "<<state<<endl;
+	}
+	return 0;
+} 
